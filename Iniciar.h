c5f4e8a0c463ae2166dc49853a151741d28ec11a
eduar3morales <edy.rocks@gmail.com>
2015-04-18 07:44:27 -0500

    xxxxxxxxx

---
+++ Iniciar.h
@@ -0,0 +1,15 @@
+//
+//  Iniciar.h
+//  MiPlacita
+//
+//  Created by Edy on 18/04/15.
+//  Copyright (c) 2015 Edy Morales. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Iniciar : NSObject
+
+
+
+@end
---
+++ Iniciar.m
@@ -0,0 +1,13 @@
+//
+//  Iniciar.m
+//  MiPlacita
+//
+//  Created by Edy on 18/04/15.
+//  Copyright (c) 2015 Edy Morales. All rights reserved.
+//
+
+#import "Iniciar.h"
+
+@implementation Iniciar
+
+@end
